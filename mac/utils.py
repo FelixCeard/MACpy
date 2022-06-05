e8f48063ee50d68c786a28550131508b254b8840
Felix Ceard-Falkenberg <felix@falkenbergs.de>
2022-06-05 20:54:46 +0200

    init

---
+++ mac/utils.py
@@ -0,0 +1,61 @@
+import os
+import os.path as path
+import subprocess
+
+import pandas as pd
+
+
+class MAC:
+
+    def __init__(self):
+
+        cwd = os.getcwd()
+        dir_name = 'MAC'
+
+        self.dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.jar_path = path.join(self.dir_path, 'MAC.jar').replace('\\', '/')
+
+        if path.isdir(path.join(cwd, dir_name)) is False:
+            print('creating dir')
+            os.mkdir(path.join(cwd, dir_name))
+
+        self.input_path = path.join(cwd, dir_name, 'temp_input.csv').replace('\\', '/')
+
+        self.cp_path = path.join(cwd, dir_name, 'temp_CP').replace('\\', '/')
+        self.runtime = path.join(cwd, dir_name, 'temp_runtime').replace('\\', '/')
+        self.data = path.join(cwd, dir_name, 'temp_data').replace('\\', '/')
+
+        self.sep = ';'
+
+        self.CLUMPS = 30
+
+        self.cp = {}
+
+    def parse_df(self, df):
+        df.to_csv(self.input_path, header=False, index=False, sep=self.sep)
+
+    def run(self, df: pd.DataFrame):
+        assert len(df.shape) == 2, "Expected a dataframe with n rows and m columns"
+
+        self.parse_df(df)  # save csv
+
+        rows = df.shape[0]
+        columns = df.shape[1] - 1  # one predictor
+
+        cmd = f'java -jar {self.jar_path} -FILE_INPUT {self.input_path} -FILE_CP_OUTPUT {self.cp_path} -FILE_RUNTIME_OUTPUT {self.runtime} -FILE_DATA_OUTPUT {self.data} -NUM_ROWS {rows} -NUM_MEASURE_COLS {columns} -FIELD_DELIMITER {self.sep} -CLUMPS {self.CLUMPS}'
+        print('mac is running...')
+        r = subprocess.run(cmd, stdout=subprocess.PIPE)
+        self.terminal_output = r.stdout.decode('utf-8')
+        print('mac has finished:')
+
+        self.parse_response()
+
+        return self.cp
+
+    def parse_response(self):
+        with open(self.cp_path, 'r') as file:
+            content = ''.join(file.readlines()).split('-------------------------------------\n')
+
+        for i, bin_str in enumerate(content):
+            c = bin_str.split('\n')[1:-1]
+            self.cp[i] = [float(n) for n in c]
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MACpy',
+    version='0.1.0',
+    author='Felix Ceard-Falkenberg',
+    author_email='felix@falkenbergs.de',
+    packages=['mac'],
+    package_data={'': ['MAC.jar']},
+    include_package_data=True,
+    description='A wrapper for the MAC algorithm',
+    install_requires=[
+        'pandas'
+    ],
+)
